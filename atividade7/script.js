fe363824f6c6d456645a04f2d690dfa7d16928b4
Adalberto Costa de Andrade Junior <56029964+AdalbertoCosta@users.noreply.github.com>
2021-03-29 12:42:10 -0300

    Add files via upload

---
+++ atividade7/script.js
@@ -0,0 +1,9 @@
+let number1 = parseFloat(prompt("Digite um número:"));
+let number2 = parseFloat(prompt("Digite um número:"));
+let number3 = parseFloat(prompt("Digite um número:"));
+
+function maiorDosTres() {
+    alert(Math.max(number1, number2, number3));
+}
+
+maiorDosTres(number1, number2, number3);
